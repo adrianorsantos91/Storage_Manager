318ee0c84b7176b691610793885aa2ed89ae558e
adrianorsantos <adriano.adm2010@gmail.com>
2022-06-01 17:10:12 -0300

    dev: started creating the productsControllers.js routes

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -1,1 +1,13 @@
-// Renomeie esse arquivo
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/product/', async (req, res) => {
+  res.status(200).json({ message: 'teste' });
+});
+
+router.get('/product/:id', async (req, res) => {
+  res.status(200).json({ message: 'teste' });
+});
+
+module.exports = router;
