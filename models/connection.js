9147268584e3bd81b2e2352b7193a36fc43fd3d5
adrianorsantos <adriano.adm2010@gmail.com>
2022-06-01 18:41:03 -0300

    fix: removed conditionally or with the information from the .env file

--- models/connection.js
+++ models/connection.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2/promise');
 require('dotenv').config(); // não se esqueça de configurar suas variáveis de ambiente aqui na configuração
 
 const connection = mysql.createPool({
-  host: process.env.MYSQL_HOST || 'localhost',
-  user: process.env.MYSQL_USER || 'trybe',
-  password: process.env.MYSQL_PASSWORD || 'Trybe@12345',
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE || 'StoreManager',
 });
 
