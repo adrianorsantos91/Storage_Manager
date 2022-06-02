c98d0d23e2821defcd73a93fea8b4ad702325936
adrianorsantos <adriano.adm2010@gmail.com>
2022-06-02 13:11:56 -0300

    dev: created the initial business rules for salescreated the initial business rules for sales

--- services/salesServices.js
+++ services/salesServices.js
@@ -1,1 +1,17 @@
-// Renomeie esse arquivo
+const salesModels = require('../models/salesModels');
+
+const getAll = async () => {
+  const [result] = await salesModels.getAll();
+  return result;
+};
+const getById = async (id) => {
+    if (id) {
+      const [result] = await salesModels.getById(id);
+      return result;
+    }
+};
+
+module.exports = {
+  getAll,
+  getById,
+};
