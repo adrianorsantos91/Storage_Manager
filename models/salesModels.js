807eceb2b2f20e6a0ec0aa606d5d41d0d2350c29
adrianorsantos <adriano.adm2010@gmail.com>
2022-06-02 13:09:56 -0300

    dev: created the initial queries in salesModels

--- models/salesModels.js
+++ models/salesModels.js
@@ -1,1 +1,10 @@
-// Renomeie esse arquivo
+const connection = require('./connection');
+
+const getAll = () => connection.execute('SELECT * FROM sales');
+
+const getById = (id) => connection.execute('SELECT * FROM sales WHERE id = ?', [id]);
+
+module.exports = {
+  getAll,
+  getById,
+};
