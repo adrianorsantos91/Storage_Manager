d53293e8d54fbd262fe2541eb3125dcacb963665
adrianorsantos <adriano.adm2010@gmail.com>
2022-06-02 14:59:48 -0300

    feat: applied the function to change object keys to camelCase pattern

--- services/salesServices.js
+++ services/salesServices.js
@@ -1,13 +1,25 @@
 const salesModels = require('../models/salesModels');
 
+const changeSalesToCamelCase = (salesData) => {
+  const { id, date, product_id, quantity } = salesData;
+
+  return {
+    saleId: id,
+    date,
+    // eslint-disable-next-line camelcase
+    productId: product_id,
+    quantity,
+  };
+};
+
 const getAll = async () => {
   const [result] = await salesModels.getAll();
-  return result;
+  return result.map(changeSalesToCamelCase);
 };
 const getById = async (id) => {
     if (id) {
       const [result] = await salesModels.getById(id);
-      return result;
+      return result.map(changeSalesToCamelCase);
     }
 };
 
