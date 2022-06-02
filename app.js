c944d6f6691e298d24ea30a22d193d96bf98d106
adrianorsantos <adriano.adm2010@gmail.com>
2022-06-02 19:57:34 -0300

    feat: finished requirement 3 and reinstalled npm

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const errorHandle = require('./middlewares/errorHandle');
 
 const app = express();
 
+app.use(express.json());
+
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
