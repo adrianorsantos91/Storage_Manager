dd2b043a0cbfe889e3c0a3dc5a6be09099870edd
adrianorsantos <adriano.adm2010@gmail.com>
2022-06-02 00:37:44 -0300

    dev: included error exception middleware in base

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const productsRouters = require('./routers/productsRouters');
+const errorHandle = require('./middlewares/errorHandle');
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.get('/', (_request, response) => {
 
 app.use('/products', productsRouters);
 
+app.use(errorHandle);
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
 // você deve usar o arquivo index.js para executar sua aplicação
