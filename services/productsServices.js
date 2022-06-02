de243f221bac83e493ac8390e7bb0b7ad161a03a
adrianorsantos <adriano.adm2010@gmail.com>
2022-06-02 01:09:44 -0300

    feat: business rule to get all products

--- services/productsServices.js
+++ services/productsServices.js
@@ -1,1 +1,11 @@
-// Renomeie esse arquivo
+const productsModels = require('../models/productsModels');
+
+const getAll = async () => {
+  const [result] = await productsModels.getAll();
+  console.log('Result: %s', result);
+  return result;
+};
+
+module.exports = {
+  getAll,
+};
