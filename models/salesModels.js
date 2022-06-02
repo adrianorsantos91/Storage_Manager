b1debbd8b0f2d6adde5fc2746871ed7c975ff1f8
adrianorsantos <adriano.adm2010@gmail.com>
2022-06-02 14:54:21 -0300

    fix: queries to filter and unify the correct data according to conditions

--- models/salesModels.js
+++ models/salesModels.js
@@ -1,8 +1,26 @@
 const connection = require('./connection');
 
-const getAll = () => connection.execute('SELECT * FROM sales');
+const getAll = () => connection.execute(
+  `SELECT
+    id,
+    date,
+    product_id,
+    quantity
+  FROM StoreManager.sales, StoreManager.sales_products
+  WHERE id = sale_id
+  ORDER BY sales.id`,
+);
 
-const getById = (id) => connection.execute('SELECT * FROM sales WHERE id = ?', [id]);
+const getById = (id) => connection.execute(
+  `SELECT
+    id,
+    date,
+    product_id,
+    quantity
+  FROM StoreManager.sales, StoreManager.sales_products
+  WHERE id = sale_id AND id = ?
+  ORDER BY sales.id`, [id],
+);
 
 module.exports = {
   getAll,
