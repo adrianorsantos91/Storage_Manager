6fff4293eb0c0083ab942f7786f7785631d6053b
adrianorsantos <adriano.adm2010@gmail.com>
2022-06-02 19:50:40 -0300

    dev: including validations in the  route

--- routers/productsRouters.js
+++ routers/productsRouters.js
@@ -1,10 +1,14 @@
 const express = require('express');
 
 const productsControllers = require('../controllers/productsControllers');
+const { validateNameProductsMiddleware,
+  validateQuantityProductsMiddleware } = require('../middlewares/productMiddleware');
 
 const router = express.Router();
 
-router.get('/:id', productsControllers.getById);
 router.get('/', productsControllers.getAll);
+router.get('/:id', productsControllers.getById);
+router.post('/', validateNameProductsMiddleware,
+validateQuantityProductsMiddleware, productsControllers.create);
 
 module.exports = router;
