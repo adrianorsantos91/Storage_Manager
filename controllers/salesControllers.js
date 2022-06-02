98045a4fc6017b9094c7606564d91584b6492e46
adrianorsantos <adriano.adm2010@gmail.com>
2022-06-02 13:15:10 -0300

    dev: created sales requisitions

--- controllers/salesControllers.js
+++ controllers/salesControllers.js
@@ -1,1 +1,23 @@
-// Renomeie esse arquivo
+const salesService = require('../services/salesServices');
+
+const getAll = async (_req, res) => {
+  const salesList = await salesService.getAll();
+  res.status(200).json(salesList);
+};
+
+const getById = async (req, res) => {
+  const { id } = req.params;
+
+  const [saleId] = await salesService.getById(id);
+
+  if (!saleId) {
+    res.status(404).json({ message: 'Product not found' });
+  }
+
+  return res.status(200).json(saleId);
+};
+
+module.exports = {
+  getAll,
+  getById,
+};
