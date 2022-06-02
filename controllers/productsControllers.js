d21cad21c0986384e5650329d6dfecef41d6b237
adrianorsantos <adriano.adm2010@gmail.com>
2022-06-02 01:13:02 -0300

    feat: function with return of the list of all products with status 200

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -1,23 +1,34 @@
-const getAll = (req, res) => {
-  res.status(200).json({ message: 'Controller' });
+const productsService = require('../services/productsServices');
+
+const getAll = async (req, res) => {
+  const result = await productsService.getAll();
+  res.status(200).json(result);
 };
 
 const getById = (req, res, next) => {
-  // const errorMessage = {
-  //   status: 200,
-  //   message: 'Funcionou',
-  // };
-
   try {
     res.status(200).json({ message: 'ControllerId' });
   } catch (error) {
     next(error);
   }
-
-  // throw error;
 };
 
 module.exports = {
   getAll,
   getById,
 };
+
+// const getById = (req, res, next) => {
+//   // const errorMessage = {
+//   //   status: 200,
+//   //   message: 'Funcionou',
+//   // };
+
+//   try {
+//     res.status(200).json({ message: 'ControllerId' });
+//   } catch (error) {
+//     next(error);
+//   }
+
+//   // throw error;
+// };
