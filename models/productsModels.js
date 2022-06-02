399d832a384e0b414d3d95ca0f2fbe818eccd29a
adrianorsantos <adriano.adm2010@gmail.com>
2022-06-02 19:38:57 -0300

    dev: including the product insertion query

--- models/productsModels.js
+++ models/productsModels.js
@@ -1,10 +1,27 @@
 const connection = require('./connection');
 
-const getAll = () => connection.execute('SELECT * FROM products');
+const getAll = () => {
+  console.log('teste');
+  return connection.execute('SELECT * FROM products');
+};
 
 const getById = (id) => connection.execute('SELECT * FROM products WHERE id = ?', [id]);
+
+const create = async (name, quantity) => {
+  const [result] = await connection.execute(
+    'INSERT INTO products (name, quantity) VALUES (?, ?)',
+    [name, quantity],
+  );
+
+  return {
+    id: result.insertId,
+    name,
+    quantity,
+  };
+};
 
 module.exports = {
   getAll,
   getById,
+  create,
 };
