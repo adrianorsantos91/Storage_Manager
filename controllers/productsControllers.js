fb80be38da993a239215f8fb91fe42a210ec5eb0
adrianorsantos <adriano.adm2010@gmail.com>
2022-06-02 19:55:25 -0300

    dev: included the request to add new products to the database

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -1,6 +1,7 @@
 const productsService = require('../services/productsServices');
 
 const getAll = async (_req, res) => {
+  console.log('all');
   const productsList = await productsService.getAll();
   res.status(200).json(productsList);
 };
@@ -17,9 +18,25 @@ const getById = async (req, res) => {
   return res.status(200).json(productsList);
 };
 
+const create = async (req, res) => {
+  const { name } = req.body;
+  const productList = await productsService.getAll();
+
+  productList.forEach((e) => {
+    console.log(e.name);
+    if (e.name === name) {
+      return res.status(409).json({ message: 'Product already exists' });
+    }
+  });
+
+  const product = await productsService.create(req.body);
+  res.status(200).json(product);
+};
+
 module.exports = {
   getAll,
   getById,
+  create,
 };
 
 // const getById = (req, res, next) => {
