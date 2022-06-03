8f63501f33137895975595638cf4c55ba261a0b5
adrianorsantos <adriano.adm2010@gmail.com>
2022-06-03 05:21:08 -0300

    dev: creation of sales validation middleware

---
+++ middlewares/salesMiddleware.js
@@ -0,0 +1,29 @@
+const validateProductIdMiddleware = (req, res, next) => {
+  const { productId } = req.body;
+   if (!productId) {
+     return res.status(400).send({ message: '"productId" is required' });
+   }
+
+   next();
+};
+
+const validateQuantityProductsMiddleware = (req, res, next) => {
+  const { quantity } = req.body;
+if (!quantity) {
+  return res.status(400).send({ message: '"quantity" is required' });
+ }
+
+ // typeof quantity === 'number'
+ if (!Number.isInteger(quantity) || quantity <= 0) {
+  return res.status(422).send(
+    { message: '"quantity" must be greater than or equal to 1' },
+  );
+ }
+
+ next();
+};
+
+module.exports = {
+  validateProductIdMiddleware,
+  validateQuantityProductsMiddleware,
+ };
