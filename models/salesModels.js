2ef2460ad1538ef04fbc0b5a07f96077092ac843
adrianorsantos <adriano.adm2010@gmail.com>
2022-06-03 19:42:32 -0300

    dev: added a function to enter new sales

--- models/salesModels.js
+++ models/salesModels.js
@@ -1,27 +1,51 @@
 const connection = require('./connection');
 
 const getAll = () => connection.execute(
-  `SELECT
-    id,
-    date,
-    product_id,
-    quantity
+  `SELECT id, date, product_id, quantity
   FROM StoreManager.sales, StoreManager.sales_products
   WHERE id = sale_id
   ORDER BY sales.id`,
 );
 
-const getById = (id) => connection.execute(
+const getById = async (id) => {
+  console.log('id', id);
+  const result = await connection.execute(
   `SELECT
-    date,
-    product_id,
-    quantity
-  FROM StoreManager.sales, StoreManager.sales_products
-  WHERE id = sale_id AND id = ?
-  ORDER BY sales.id`, [id],
+    s.id,
+    s.date,
+    sl.product_id,
+    sl.quantity
+  FROM StoreManager.sales AS s, StoreManager.sales_products AS sl
+  WHERE s.id = sl.sale_id AND s.id = ?
+  ORDER BY s.id`, [id],
 );
+
+console.log('result', result);
+return result;
+};
+
+const create = async (salesData) => {
+  const [result] = await connection.execute(
+    'INSERT INTO sales (date) VALUES (now())',
+  );
+
+  const idSale = result.insertId;
+
+  await salesData.forEach(({ productId, quantity }) => {
+    connection.execute(
+      'INSERT INTO sales_products (sale_id, product_id, quantity) VALUES (?, ?, ?)',
+      [idSale, productId, quantity],
+    );
+  });
+
+  return {
+    id: +idSale,
+    itemsSold: salesData,
+  };
+};
 
 module.exports = {
   getAll,
   getById,
+  create,
 };
