37779b40b663ad8de808c49f64e0a5296b9fc55d
adrianorsantos <adriano.adm2010@gmail.com>
2022-06-07 21:43:46 -0300

    dev: created mock directory

---
+++ Mock/productsMock.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+  id: 1,
+  name: 'Martelo de Thor',
+  quantity: 10,
+  },
+  {
+    id: 2,
+    name: 'Traje de encolhimento',
+    quantity: 20,
+  },
+  {
+    id: 3,
+    name: 'Escudo do Capitão América',
+    quantity: 30,
+  },
+];
---
+++ Mock/salesMock.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    saleId: 1,
+    date: '2022-06-07T06:52:24.000Z',
+    productId: 1,
+    quantity: 5,
+  },
+  {
+    saleId: 1,
+    date: '2022-06-07T06:52:24.000Z',
+    productId: 2,
+    quantity: 10,
+  },
+  {
+    saleId: 2,
+    date: '2022-06-07T06:52:24.000Z',
+    productId: 3,
+    quantity: 15,
+  },
+];
