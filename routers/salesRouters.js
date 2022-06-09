17d5db61da56db77ea0f922867f9b3be5b290ea4
adrianorsantos <adriano.adm2010@gmail.com>
2022-06-09 01:31:51 -0300

    dev: created the route /sales/:id with the DELETE verb

--- routers/salesRouters.js
+++ routers/salesRouters.js
@@ -8,5 +8,6 @@ router.get('/:id', salesControllers.getById);
 router.get('/', salesControllers.getAll);
 router.post('/', validateSalesMiddleware, salesControllers.create);
 router.put('/:id', validateSalesMiddleware, salesControllers.updateById);
+router.delete('/:id', salesControllers.deleteById);
 
 module.exports = router;
