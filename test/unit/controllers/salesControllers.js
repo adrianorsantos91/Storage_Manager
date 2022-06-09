f3e2979853442bf83f363aac5a8bb80ecaf17496
adrianorsantos <adriano.adm2010@gmail.com>
2022-06-09 16:26:46 -0300

    test: 100% test coverage in salesControllers

--- test/unit/controllers/salesControllers.js
+++ test/unit/controllers/salesControllers.js
@@ -5,7 +5,7 @@ const salesServices = require('../../../services/salesServices');
 const salesControllers = require('../../../controllers/salesControllers');
 const salesMock = require('../../../Mock/salesMock');
 
-describe('1 - Busca todos os dados de vendas no DB [Controllers - Product]', async () => {
+describe('1 - Busca todos os dados de vendas no DB [Controllers - Sales]', async () => {
   const response = {};
     const request = {};
 
@@ -35,41 +35,112 @@ describe('1 - Busca todos os dados de vendas no DB [Controllers - Product]', asy
     });
 });
 describe('2 - Busca todos os dados das produtos por id no DB', async () => {
+  describe("quando é inserido sem sucesso", async () => {
+    const response = {};
+    const request = {};
+
+    before(() => {
+      request.body = {};
+      request.params = { id: 1};
+
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns();
+
+      sinon.stub(salesServices, 'getById').resolves(false);
+    })
+
+    after(() => {
+      salesServices.getById.restore();
+    });
+
+    it('é chamado o método "status" passando o código 404', async () => {
+      await salesControllers.getById(request, response);
+
+      expect(response.status.calledWith(404)).to.be.equal(true);
+    });
+
+    it('é chamado o json com a mensagem "Sale not found"', async () => {
+      await salesControllers.getById(request, response);
+
+      expect(response.json.calledWith({ message: 'Sale not found' })).to.be.equal(true);
+    });
+
+  });
   describe("quando é inserido com sucesso", async () => {
     const response = {};
     const request = {};
 
     before(() => {
       request.body = {};
+      request.params = { id: 1};
 
       response.status = sinon.stub().returns(response);
       response.json = sinon.stub().returns();
 
-      sinon.stub(salesServices, 'getAll').resolves({
-        "id": 1,
-        "name": "Martelo de Thor",
-        "quantity": 10
-      });
+      sinon.stub(salesServices, 'getById').resolves([
+        {
+          "date": "2022-06-09T16:32:54.000Z",
+          "productId": 1,
+          "quantity": 5
+        },
+        {
+          "date": "2022-06-09T16:32:54.000Z",
+          "productId": 2,
+          "quantity": 10
+        }
+      ]);
     })
 
     after(() => {
-      salesServices.getAll.restore();
+      salesServices.getById.restore();
     });
 
     it('é chamado o método "status" passando o código 200', async () => {
-      await salesControllers.getAll(request, response);
+      await salesControllers.getById(request, response);
 
       expect(response.status.calledWith(200)).to.be.equal(true);
     });
 
-    it('é chamado o método "json" passando um object', async () => {
-      await salesControllers.getAll(request, response);
+    it('é chamado o método "json" passando um array', async () => {
+      await salesControllers.getById(request, response);
 
-      expect(response.json.calledWith(sinon.match.object)).to.be.equal(true);
+      expect(response.json.calledWith(sinon.match.array)).to.be.equal(true);
     });
   });
 });
 describe('3 - Insere novos produtos no DB', async () => {
+  describe("quando é inserido sem sucesso", async () => {
+    const response = {};
+    const request = {};
+
+    before(() => {
+      request.body = {
+        name: "Gavião",
+        quantity: 1
+      };
+
+      response.status = sinon.stub().returns(response);
+      response.send = sinon.stub().returns();
+
+      sinon.stub(salesServices, "create").resolves(false);
+    });
+
+    after(() => {
+      salesServices.create.restore();
+    });
+
+    it("é chamado o status com o código 400", async () => {
+      await salesControllers.create(request, response);
+
+      expect(response.status.calledWith(400)).to.be.equal(true);
+    });
+
+    it("é chamado o json com a mensagem 'Venda não adicionada'", async () => {
+      await salesControllers.create(request, response);
+
+      expect(response.send.calledWith('Venda não adicionada')).to.be.equal(true);
+    });
+  });
   describe("quando é inserido com sucesso", async () => {
     const response = {};
     const request = {};
@@ -81,7 +152,6 @@ describe('3 - Insere novos produtos no DB', async () => {
       };
 
       response.status = sinon.stub().returns(response);
-      // response.send = sinon.stub().returns();
       response.json = sinon.stub().returns();
 
       sinon.stub(salesServices, "create").resolves(true);
@@ -93,13 +163,40 @@ describe('3 - Insere novos produtos no DB', async () => {
 
     it("é chamado o status com o código 201", async () => {
       await salesControllers.create(request, response);
-
       expect(response.status.calledWith(201)).to.be.equal(true);
     });
   });
 });
 
-describe('4 - Atualizar as vendas no DB', async  () => {
+describe('4 - Atualizar as vendas pelo id no DB', async  () => {
+  describe('quando é deletado sem sucesso', () => {
+    const response = {};
+    const request = {};
+
+    before(() => {
+      request.params = { id: 1};
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns();
+
+      sinon.stub(salesServices, 'updateById').resolves(false);
+    })
+
+    after(() => {
+      salesServices.updateById.restore();
+    });
+
+    it('é chamado o método "status" passando o código 404', async () => {
+      await salesControllers.updateById(request, response);
+
+      expect(response.status.calledWith(404)).to.be.equal(true);
+    });
+
+    it('é chamado o send com a mensagem "Sale not found"', async () => {
+      await salesControllers.updateById(request, response);
+      expect(response.json.calledWith({ message: 'Sale not found' })).to.be.equal(true);
+    });
+  })
+  describe('quando é atualizado com sucesso', () => {
     const response = {};
     const request = {};
 
@@ -132,34 +229,63 @@ describe('4 - Atualizar as vendas no DB', async  () => {
       await salesControllers.updateById(request, response);
       expect(response.json.calledWith(sinon.match.object)).to.be.equal(true);
     });
+  });
 });
 
 describe('5 - Delete produtos no DB', () => {
-  const response = {};
-    const request = {};
+  describe('quando é deletado sem sucesso', () => {
+    const response = {};
+      const request = {};
 
-    before(() => {
-      request.params = { id: 1};
-      response.status = sinon.stub().returns(response);
-      response.json = sinon.stub().returns();
-      response.end = sinon.stub().returns();
+      before(() => {
+        request.params = { id: 1};
+        response.status = sinon.stub().returns(response);
+        response.json = sinon.stub().returns();
 
-      sinon.stub(salesServices, 'deleteById').resolves({});
-    })
+        sinon.stub(salesServices, 'deleteById').resolves(false);
+      })
 
-    after(() => {
-      salesServices.deleteById.restore();
-    });
+      after(() => {
+        salesServices.deleteById.restore();
+      });
 
-    it('é chamado o método "status" passando o código 204', async () => {
-      await salesControllers.deleteById(request, response);
+      it('é chamado o método "status" passando o código 404', async () => {
+        await salesControllers.deleteById(request, response);
 
-      expect(response.status.calledWith(204)).to.be.equal(true);
-    });
+        expect(response.status.calledWith(404)).to.be.equal(true);
+      });
 
-    it('é chamado o send com a mensagem "Dados inválidos"', async () => {
-      await salesControllers.deleteById(request, response);
-      console.log(response.end);
-      expect(response.end.calledWith()).to.be.equal(true);
-    });
+      it('é chamado o send com a mensagem "Sale not found"', async () => {
+        await salesControllers.deleteById(request, response);
+        expect(response.json.calledWith({ message: 'Sale not found' })).to.be.equal(true);
+      });
+  })
+  describe('quando é deletado com sucesso', () => {
+    const response = {};
+      const request = {};
+
+      before(() => {
+        request.params = { id: 1};
+        response.status = sinon.stub().returns(response);
+        response.json = sinon.stub().returns();
+        response.end = sinon.stub().returns();
+
+        sinon.stub(salesServices, 'deleteById').resolves({});
+      })
+
+      after(() => {
+        salesServices.deleteById.restore();
+      });
+
+      it('é chamado o método "status" passando o código 204', async () => {
+        await salesControllers.deleteById(request, response);
+
+        expect(response.status.calledWith(204)).to.be.equal(true);
+      });
+
+      it('é chamado o send com a mensagem "Dados inválidos"', async () => {
+        await salesControllers.deleteById(request, response);
+        expect(response.end.calledWith()).to.be.equal(true);
+      });
+  })
 });
